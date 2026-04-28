eba2d38902dfb254f9e7a17d92282af018f0a0e8
RigobertoVargasGaona <rigovg1054@gmamil.com>
2026-04-27 19:35:27 -0500

    feat: add createProduct and createProductStepsDefinitions classes for product creation feature

---
+++ src/test/java/co/com/runners/createProduct.java
@@ -0,0 +1,17 @@
+package co.com.runners;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(
+        features = "src/test/resources/features/createProduct.feature",
+        glue = {"co.com.stepsdefinitions"},
+        plugin = {"pretty", "html:target/cucumber-reports"},
+        snippets = SnippetType.CAMELCASE
+)
+
+public class createProduct {
+}
