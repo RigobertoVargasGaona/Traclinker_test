aa1139476b1218290c52642802158959254015de
DevJuan001 <juanesyt7@gmail.com>
2026-05-11 20:11:28 -0500

    feat: add crearUsuario step definitions for Cucumber tests

--- src/test/java/co/com/test/stepsdefinitions/crearUsuarioStepsDefinitions.java
+++ src/test/java/co/com/test/stepsdefinitions/crearUsuarioStepsDefinitions.java
@@ -6,7 +6,7 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
-public class createUserStepsDefinitions {
+public class crearUsuarioStepsDefinitions {
 
 
     @Given("^the administrator is authenticated in the system$")
