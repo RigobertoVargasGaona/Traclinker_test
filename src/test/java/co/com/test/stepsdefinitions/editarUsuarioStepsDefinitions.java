c3e07acf32b54f34cd6b3d2304c27367d17dd91e
DevJuan001 <juanesyt7@gmail.com>
2026-05-11 20:11:33 -0500

    feat: add editarUsuario step definitions for Cucumber tests

--- src/test/java/co/com/test/stepsdefinitions/editarUsuarioStepsDefinitions.java
+++ src/test/java/co/com/test/stepsdefinitions/editarUsuarioStepsDefinitions.java
@@ -6,7 +6,7 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
-public class editUserStepsDefinitions {
+public class editarUsuarioStepsDefinitions {
 
 
     @Given("^The user is on the profile screen\\.$")
