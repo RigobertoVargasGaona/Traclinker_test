d5dff7c040ff0291c2b42d03726da17f26262571
DevJuan001 <juanesyt7@gmail.com>
2026-05-11 20:11:04 -0500

    feat: add crearProducto runner for Cucumber tests

--- src/test/java/co/com/test/runners/crearProducto.java
+++ src/test/java/co/com/test/runners/crearProducto.java
@@ -7,11 +7,11 @@ import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
-        features = "src/test/resources/features/createProduct.feature",
+        features = "src/test/resources/features/crearProducto.feature",
         glue = {"co.com.stepsdefinitions"},
         plugin = {"pretty", "html:target/cucumber-reports"},
         snippets = SnippetType.CAMELCASE
 )
 
-public class createProduct {
+public class crearProducto {
 }
