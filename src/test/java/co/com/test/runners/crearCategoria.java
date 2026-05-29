db97e421326b6efad9809700875ca14925f9e72d
MichaelGLindo <maicolflass10@gmail.com>
2026-05-29 00:47:55 -0500

    feat: agregar clase crearCategoria para manejar pruebas de creación de categorías

---
+++ src/test/java/co/com/test/runners/crearCategoria.java
@@ -0,0 +1,17 @@
+package co.com.test.runners;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(
+        features = "src/test/resources/features/categoria.feature",
+        glue = {"co.com.stepsdefinitions"},
+        plugin = {"pretty", "html:target/cucumber-reports"},
+        snippets = SnippetType.CAMELCASE
+)
+
+public class crearCategoria {
+}
