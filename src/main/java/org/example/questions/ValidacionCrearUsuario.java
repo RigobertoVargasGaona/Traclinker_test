734c9dcaebb158bb89b04e71d817d64079fa3601
DevJuan001 <juanesyt7@gmail.com>
2026-05-28 20:02:43 -0500

    feat: enhance ValidacionCrearUsuario to wait for confirmation message visibility before validation

--- src/main/java/org/example/questions/ValidacionCrearUsuario.java
+++ src/main/java/org/example/questions/ValidacionCrearUsuario.java
@@ -3,9 +3,12 @@ package org.example.questions;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Question;
 import net.serenitybdd.screenplay.questions.Text;
+import net.serenitybdd.screenplay.waits.WaitUntil;
+import org.example.userinterfaces.PaginaUsuarios;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.isVisible;
 import static org.example.userinterfaces.PaginaUsuarios.MENSAJE_DE_CONFIRMACION;
 
 public class ValidacionCrearUsuario implements Question<Boolean> {
@@ -21,6 +24,8 @@ public class ValidacionCrearUsuario implements Question<Boolean> {
     @Override
     public Boolean answeredBy(Actor actor) {
         try {
+            WaitUntil.the(PaginaUsuarios.MENSAJE_DE_CONFIRMACION, isVisible())
+                    .forNoMoreThan(2).seconds();
             String texto = Text.of(MENSAJE_DE_CONFIRMACION).viewedBy(actor).asString().trim();
             logger.info("Texto obtenido: {}", texto);
             return MENSAJE_ESPERADO.equalsIgnoreCase(texto);
