95fb0a8d87844667905094739bef6db039009dfc
DevJuan001 <juanesyt7@gmail.com>
2026-06-22 12:54:42 -0500

    chore: update default URL in Inicio class to new project deployment address

--- src/main/java/org/example/userinterfaces/Inicio.java
+++ src/main/java/org/example/userinterfaces/Inicio.java
@@ -3,7 +3,7 @@ package org.example.userinterfaces;
 import net.serenitybdd.core.pages.PageObject;
 import net.thucydides.core.annotations.DefaultUrl;
 
-@DefaultUrl("https://tracklinker-frontend-web.vercel.app/")
+@DefaultUrl("https://tracklinker-frontend-web-project.onrender.com/")
 
 public class Inicio extends PageObject {
 }
